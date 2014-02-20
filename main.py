9a1195b0f84a318306e7a5b7dcbaa6121c955ef1
Luu <luu@test.cz>
2014-02-20 14:06:53 +0100

    Skript upraven tak, aby umel zpracovat libovolne mnozstvi snimacu v cyklu s vyjimkou pro snimani dat z internetu.

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ import weather
 import time
 import sqlite3
 
-# Zjistime si aktualni teplotu z internetu
-tempreature, condition = weather.weatherInfo('11782')
-
-# Odstranime jednotku z teploty
-tempreature = tempreature.rstrip('C')
-
 # Zjistime aktualni datum a cas v odpovisajicich formatech
 # pro jednotlive tabulky
 now = time.strftime('%Y-%m-%d %H:%M:%S')
@@ -17,20 +11,42 @@ day = time.strftime('%Y-%m-%d')
 month = time.strftime('%Y-%m')
 year = time.strftime('%Y')
 
-# Zapiseme aktualni teplotu a stav do databaze
+# Pro kazdou zkoumanou lokalitu (internet, ci senzory)
+# provedeme zaznam a prepocet
+sensors = ['outside - internet',
+          ['inside - terrarium', '81.7FD921000000'],
+          ['inside - livingroom', '83.7FD921000000']]
 
-# Pripojeni a kurzor
-conn = sqlite3.connect('database.sqlite')
-c = conn.cursor()
+for position in sensors:
 
-# Tuple s daty pro zapis
-data = (now, 'outside - internet', tempreature, condition)
+    # Pokud se jena o internetovou teplotu, zjistime info z netu
+    # jinak zjistime ze senzoru teploty
+    if position == 'outside - internet':
+        # Zjistime si aktualni teplotu z internetu
+        tempreature, condition = weather.weatherInfo('11782')
 
-# SQL dotaz
-c.execute('INSERT INTO records VALUES (?, ?, ?, ?)', data)
+        # Odstranime jednotku z teploty
+        tempreature = tempreature.rstrip('C')
 
-# Commit
-conn.commit()
+        # Zapiseme aktualni teplotu a stav do databaze
 
-# Uzavreme spojeni s databazi
-conn.close()
+        # Pripojeni a kurzor
+        conn = sqlite3.connect('database.sqlite')
+        c = conn.cursor()
+
+        # Tuple s daty pro zapis
+        data = (now, position, tempreature, condition)
+
+        # SQL dotaz
+        c.execute('INSERT INTO records VALUES (?, ?, ?, ?)', data)
+
+        # Commit
+        conn.commit()
+
+        # Uzavreme spojeni s databazi
+        conn.close()
+    else:
+        # Zde bude nasledovat totez pro senzory teploty
+        name, address = position
+        print 'Sensor name: %s Sensor adress: %s' % (name, address)
+        pass
