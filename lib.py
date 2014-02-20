4f30e672e4d7e68c04cada8a0f29148920772016
Luu <luu@test.cz>
2014-02-20 14:57:41 +0100

    Pridany komentare k funkcim.

--- lib.py
+++ lib.py
@@ -3,12 +3,14 @@
 from collections import Counter
 
 
+# Pro zadanou podminku vybere z db prumernou teplotu
 def get_avg_temp(cursor, cond):
     cursor.execute('SELECT avg(tempreature) as average FROM records \
                     WHERE date like ?', (cond + ' %', ))
     return cursor.fetchone()[0]
 
 
+# Pro zadanou podminku vybere z db nejcastejsi stav pocasi
 def get_avg_condition(cursor, cond):
     cursor.execute('SELECT condition FROM records \
                     WHERE date like ?', (cond + ' %', ))
