5136490a7acf916de5e5a8f772d7ae5e1aeb91de
frenzymadness <luu@test.cz>
2014-02-21 19:48:21 +0100

    Upravena struktura tabulek tak, aby primarni klic obsahoval datum i pozici a tim byl lepe prizpusoben pro vice snimacu.

--- database.sql
+++ database.sql
@@ -2,9 +2,9 @@
 --CREATE DATABASE 'tempreatures';
 
 -- Main table for all records
-CREATE TABLE records(date CHAR(15) PRIMARY KEY, position CHAR(100) NOT NULL, tempreature REAL NOT NULL, condition CHAR(150));
+CREATE TABLE records(date CHAR(15), position CHAR(100) NOT NULL, tempreature REAL NOT NULL, condition CHAR(150));
 
 -- Tables for averages values for day, month and year
-CREATE TABLE days(date CHAR(15) PRIMARY KEY, position CHAR(100) NOT NULL, tempreature REAL NOT NULL, condition CHAR(150));
-CREATE TABLE months(date CHAR(15) PRIMARY KEY, position CHAR(100) NOT NULL, tempreature REAL NOT NULL, condition CHAR(150));
-CREATE TABLE years(date CHAR(15) PRIMARY KEY, position CHAR(100) NOT NULL, tempreature REAL NOT NULL, condition CHAR(150));
+CREATE TABLE days(date CHAR(15), position CHAR(100) NOT NULL, tempreature REAL NOT NULL, condition CHAR(150), PRIMARY KEY (date, position));
+CREATE TABLE months(date CHAR(15), position CHAR(100) NOT NULL, tempreature REAL NOT NULL, condition CHAR(150), PRIMARY KEY (date, position));
+CREATE TABLE years(date CHAR(15), position CHAR(100) NOT NULL, tempreature REAL NOT NULL, condition CHAR(150), PRIMARY KEY (date, position));
